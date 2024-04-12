a5581f01e88d8f7643be66f45c8c2356e292cc4e
Byben3126 <clement.guilloux3126@gmail.com>
2024-04-12 15:08:13 +0200

    fix encore et encore

--- migrations/Version20240412130754.php
+++ migrations/Version20240412130754.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240412125650 extends AbstractMigration
+final class Version20240412130754 extends AbstractMigration
 {
     public function getDescription(): string
     {
